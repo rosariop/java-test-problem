67d43fbb953e3886c7c1cd03b1a07c5428149992
rosario polito <rosario.polito@gmx.de>
2023-07-19 21:58:45 +0200

    adding todo

--- src/main/java/com/example/demo/SomeClass.java
+++ src/main/java/com/example/demo/SomeClass.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class SomeClass {
 
-    @Value("${config.key}")
+    @Value("${config.key}") // TODO: How to mock this?
     private String key;
 
     @GetMapping
